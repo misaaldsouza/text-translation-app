5b66b85e33b8b9e34b293feb103c57fa8e826a55
misaaldsouza <52240946+misaaldsouza@users.noreply.github.com>
2022-02-07 21:59:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ import pickle
 import numpy as np
 import pandas as pd
 
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system(f"dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 app = FastAPI()
 
 class TranslationModel(BaseModel):
